13abf42391ee2add33427cfeba5a6c628f3f2cde
Mark Fairhurst <mark@fairhursts.net>
2022-09-27 17:16:21 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  // console.log(import.meta.env.VITE_SOME_KEY)
+
+  return (
+    <div className="App">
+      <h1>BCC Rides</h1>
+      <div>
+        More to come...
+      </div>
+    </div>
+  )
+}
+
+export default App
